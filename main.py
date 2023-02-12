12315ef4580ffb333f21eed1af3800dc75be60e2
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-12 20:23:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,114 @@
+import streamlit as st
+from datetime import datetime
+
+def vallen_skivstång():
+    st.markdown("""
+    **Vallen Skivstång 60 min + bastu 17:30-19:30**
+    - Benböj 5x5 (90kg)
+    - Marklyft 5x5 (120kg)
+    - Frivändningar 5x5 (67.5kg)
+    - Bänkpress 5x5 (75kg)
+    """)
+
+def stretch():
+    st.markdown("""
+    **Valfri stretch 15 min 07:15-07:30**
+    - Morgonrutin från den där duden
+    - Yin-yoga övning där man lutar sig bakåt
+    """)
+
+def meditation():
+    st.markdown("""
+    **Meditation 15 min 07:45-08:15**
+    - Klä dig varmt
+    - Gå till berget och meditera i 15 min
+    - Ta med mobil för att ta tid
+    """)
+
+def sats_cardio():
+    st.markdown(
+    """
+    **Gym Cardio 40/20 x2 06:45-07:15**
+    - Burpees 
+    - KB Swings (32kg)
+    - Hoppa på box
+    - Slam balls (12kg)
+    - PU Deadlift (10 kg)
+    - KB Snactch (20kg)
+    - Barbell Thrusters (30kg)
+    - Boll över axel (30kg)
+    - Deadlift + Clean (30kg)
+    - Planka
+    """)
+
+def chins():
+    st.markdown(
+    """
+    **Chins x20 under 10-15 minuter valfri tid**
+    - 4 - 3 - 3
+    - 4 - 3 - 3
+    """)
+
+def armhävningar():
+    st.markdown(
+    """
+    **Armhävningar x200 under valfri tid**
+    - 20 x 10
+    """)
+
+def get_week():
+    return datetime.isocalendar(datetime.now()).week
+
+st.header(f"Schema Vecka 7, 8 & 9")
+st.markdown("*Sista träningsdag: 3e mars*")
+
+with st.expander("Måndag"):
+    sats_cardio()
+    st.write("")
+
+    meditation()
+    st.write("")
+
+    vallen_skivstång()
+    st.write("")
+
+with st.expander("Tisdag"):
+    stretch()
+    st.write("")
+
+    meditation()
+    st.write("")
+
+    chins()
+    st.write("")
+
+with st.expander("Onsdag"):
+    sats_cardio()
+    st.write("")
+
+    meditation()
+    st.write("")
+
+    armhävningar()
+    st.write("")
+
+with st.expander("Torsdag"):
+    stretch()
+    st.write("")
+    
+    meditation()
+    st.write("")
+
+    chins()
+    st.write("")
+
+with st.expander("Fredag"):
+    sats_cardio()
+    st.write("")
+
+    meditation()
+    st.write("")
+
+    vallen_skivstång()
+    st.write("")
+
---
+++ pages/01_Övningar.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.subheader("Thruster")
+st.video("https://www.youtube.com/watch?v=aea5BGj9a8Y")
