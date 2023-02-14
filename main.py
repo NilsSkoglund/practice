45043b1e3e90c5e6e6487c709e723b9181d66838
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-14 11:12:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def add_exercise_func():
     st.write("HAJ FROM FUNC")
     key_string = f"Övning: {st.session_state['exercise_counter']}"
     temp_dct = {key_string: st.session_state[key_string]}
+    st.write(key_string)
+    st.write(temp_dct)
+
     st.session_state["db"].put({"Övningar": temp_dct}
                                 , key = st.session_state["workout name"])
     st.session_state["exercise_counter"]+=1
