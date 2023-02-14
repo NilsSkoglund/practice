8c65b7e0dc0c97fdfc872f0ade94d9602bef39c1
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-14 14:16:00 +0100

    Create 03_Schema.py

---
+++ pages/03_Schema.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from datetime import datetime
+from deta import Deta
+
+# Connect to Deta Base with your Project Key
+if "deta" not in st.session_state:
+    st.session_state["deta"] = Deta(st.secrets["deta_key"])
+
+
+# Skapa träningsvecka
+st.date_input(
+    "Veckostart",
+    datetime.date(datetime.now()))
+
+# Ange datum
+    # Ange pass 1
+        # Genomfört
+        # Kommentar
+    # Ange pass 2
+        # Genomfört
+        # Kommentar
+# Ange datum
+    # Ange pass 1
+        # Gneomfört 
+        # Kommentar
