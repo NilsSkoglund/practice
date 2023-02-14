cbeb1d124a21226cc08d7e46f34a69bb446b2245
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-14 14:46:12 +0100

    Update 03_Schema.py

--- pages/03_Schema.py
+++ pages/03_Schema.py
@@ -6,6 +6,12 @@ from deta import Deta
 if "deta" not in st.session_state:
     st.session_state["deta"] = Deta(st.secrets["deta_key"])
 
+st.session_state["current_week"] =\
+     datetime.now().date().isocalendar().week
+current_weak = st.session_state["current_week"]
+
+st.subheader(f"Veckonummer: {current_weak}")
+
 
 # Skapa träningsvecka
 st.date_input(
@@ -14,14 +20,13 @@ st.date_input(
     , key = "first_day_of_week"
 )
 
-st.session_state["current_week"] =\
-     datetime.now().date().isocalendar().week
+
 
 st.session_state["week_from_input"] =\
      st.session_state["first_day_of_week"].isocalendar().week
+week_from_input = st.session_state["week_from_input"]
 
-st.write(st.session_state["current_week"])
-st.write(st.session_state["week_from_input"])
+st.subheader(f"Vecka: {week_from_input}")
 
 # Ange datum
     # Ange pass 1
