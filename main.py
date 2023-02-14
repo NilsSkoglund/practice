fe701fcc2ddcf3c5afdf1c104d1bc470b03690e4
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-14 11:01:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ if workout_name:
     # connect to database
     # database name based on username - new session state variable
     temp_dct = {"Övningar":{}}
-    st.session_state["db"].put(temp_dct
-    , key = workout_name)
-
+    try:
+        st.session_state["db"].put(temp_dct
+        , key = workout_name)
+    except:
+        pass
     add_exercise_btn = st.button("Lägg till övning")
 
     if add_exercise_btn:
