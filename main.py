e55c084a014d80e5b65b90365ca16438af2a965c
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-14 09:41:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ with st.form("my_form"):
    st.write("Inside the form")
    slider_val = st.slider("Form slider")
    checkbox_val = st.checkbox("Form checkbox")
+   text_input = st.text_input("Form text input")
 
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
