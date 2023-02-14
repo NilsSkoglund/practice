7210486aba4ae2e81230fc4a5c03367e544c4210
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-14 15:18:50 +0100

    Update 03_Schema.py

--- pages/03_Schema.py
+++ pages/03_Schema.py
@@ -2,6 +2,22 @@ import streamlit as st
 from datetime import datetime
 from deta import Deta
 
+def list_week_days():
+    lista_veckodagar = [
+        "måndag"
+        , "tisdag"
+        , "onsdag"
+        , "torsdag"
+        , "fredag"
+        , "lördag"
+        , "söndag"
+    ]
+    for i in lista_veckodagar:
+        st.checkbox(
+            f"Lägg till pass {i}"
+            , key = f"Vecka {week_from_input} {i}"
+        )
+        
 # Connect to Deta Base with your Project Key
 if "deta" not in st.session_state:
     st.session_state["deta"] = Deta(st.secrets["deta_key"])
@@ -28,21 +44,8 @@ week_from_input = st.session_state["week_from_input"]
 
 st.subheader(f"Vecka: {week_from_input}")
 
-def list_week_days():
-    lista_veckodagar = [
-        "måndag"
-        , "tisdag"
-        , "onsdag"
-        , "torsdag"
-        , "fredag"
-        , "lördag"
-        , "söndag"
-    ]
-    for i in lista_veckodagar:
-        st.checkbox(
-            f"Lägg till pass {i}"
-            , key = f"Vecka {week_from_input} {i}"
-        )
+list_week_days()
+
 if st.session_state[f"Vecka {week_from_input} måndag"]:
     st.write("lägg till här")
 
