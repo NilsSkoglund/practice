d1b57e21c57ad89724e64168ad7019c52091a58d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-17 16:48:46 +0100

    Update 05_Kvartal.py

--- pages/05_Kvartal.py
+++ pages/05_Kvartal.py
@@ -7,7 +7,10 @@ def add_comment(quarter):
     item = db.get(quarter)
     next_comment_no = len(item["Comments"]) + 1
     #item["Comments"].update({f"Comment{next_comment_no}": st.session_state[f'add_comment{quarter}']})
-    item["Comments"].update({f"Comment{next_comment_no}": "Skriv kommentar här"})
+    item["Comments"]\
+        .update({f"Comment{next_comment_no}":\
+            "Skriv kommentar här. \
+            För att radera kommentar, ta bort all text och spara."})
     db.put(item)
 
 def modify_comment(comment, quarter):
