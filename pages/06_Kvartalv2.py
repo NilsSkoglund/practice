7fb0f1b08fb0727cb19aa053959794780e744e79
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-18 09:49:05 +0100

    Update 06_Kvartalv2.py

--- pages/06_Kvartalv2.py
+++ pages/06_Kvartalv2.py
@@ -14,7 +14,7 @@ db = st.session_state["deta"].Base("Quarterly_goals")
     # name - text input
     # specify goal - text input
 with st.form("my_form"):
-   st.write("Inside the form")
+   st.subheader("Skapa mål")
    kvartal = st.radio(
         "För vilket kvartal gäller målet?"
         , ('Q1', 'Q2', 'Q3', 'Q4')
@@ -22,5 +22,6 @@ with st.form("my_form"):
    name = st.text_input("Namn på målet")
    beskrivning = st.text_input("Beskriv ditt mål")
    datum = st.date_input("När ska målet vara uppnåt?")
+   noteringar = st.text_area("Övriga anteckningar")
 
    submitted = st.form_submit_button("Submit")
