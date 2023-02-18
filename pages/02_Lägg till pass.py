aae8adbc0e24099cbb2f23b42b38d294ba9246f6
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-18 14:21:26 +0100

    Update 02_Lägg till pass.py

--- pages/02_Lägg till pass.py
+++ pages/02_Lägg till pass.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from datetime import datetime
 from deta import Deta
-# Connect to Deta Base with your Project Key
+
+############################### Session state #################################
 if "deta" not in st.session_state:
     st.session_state["deta"] = Deta(st.secrets["deta_key"])
 
@@ -9,8 +10,9 @@ if "db" not in st.session_state:
     st.session_state["db"] =\
          st.session_state["deta"].Base("workouts")
 
+################################# Functions ###################################
 
-def add_exercise_func():
+def update_workouts_db():
     dct_from_db = st.session_state["db"]\
         .get(st.session_state["workout name"])
     
@@ -22,12 +24,7 @@ def add_exercise_func():
     st.session_state["db"].put(new_temp_dct
                     , key = st.session_state["workout name"])
 
-st.header("Lägg till pass")
-st.text_input("Ange namnet på passet du vill lägga till"
-            , key = "workout name")
-
-if st.session_state["workout name"]:
-    
+def add_exercise():
     temp_dct = {"Övningar":{}}
     
     # if key already exist, insert throws an error
@@ -47,19 +44,16 @@ if st.session_state["workout name"]:
          f"Övning {st.session_state['no_of_exercises']+1}"
     
     st.text_input("Lägg till övning"
-                , on_change=add_exercise_func
+                , on_change=update_workouts_db
                 , key=st.session_state["next_exercise"])       
    
-
     st.write(text_from_db)
-
-st.checkbox("Visa alla inlagda pass"
-            , key = "Visa inlagda pass")
 
 def remove_workout():
     st.session_state["db"].delete(st.session_state["ta bort pass"])
 
-if st.session_state["Visa inlagda pass"]:
+
+def display_inlagda_pass():
     db = st.session_state["db"]
 
     lista_pass = [i["key"] for i in db.fetch().items]
@@ -71,4 +65,18 @@ if st.session_state["Visa inlagda pass"]:
                 , options = lista_pass
                 , key = "ta bort pass"
                 , on_change=remove_workout)
-    
+
+################################## Program ####################################
+
+st.header("Lägg till pass")
+st.text_input("Ange namnet på passet du vill lägga till"
+            , key = "workout name")
+
+if st.session_state["workout name"]:
+    add_exercise()
+
+st.checkbox("Visa alla inlagda pass"
+            , key = "Visa inlagda pass")
+
+if st.session_state["Visa inlagda pass"]:
+    display_inlagda_pass()
