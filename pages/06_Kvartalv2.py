3343d875419d1b8c82991652f337ff2922df9e2b
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-18 09:54:17 +0100

    Update 06_Kvartalv2.py

--- pages/06_Kvartalv2.py
+++ pages/06_Kvartalv2.py
@@ -19,9 +19,18 @@ with st.form("my_form"):
         "För vilket kvartal gäller målet?"
         , ('Q1', 'Q2', 'Q3', 'Q4')
         , horizontal=True)
-   name = st.text_input("Namn på målet")
+   namn = st.text_input("Namn på målet")
    beskrivning = st.text_input("Beskriv ditt mål")
    datum = st.date_input("När ska målet vara uppnåt?")
    noteringar = st.text_area("Övriga anteckningar")
 
-   submitted = st.form_submit_button("Submit")
+   submitted = st.form_submit_button("Skapa mål")
+
+   if submitted:
+    key = kvartal+namn
+    temp_dct = {"namn":namn
+                , "kvartal":kvartal
+                , "beskrivning":beskrivning
+                , "datum": datum
+                , "noteringar":noteringar}
+st.write(temp_dct)
