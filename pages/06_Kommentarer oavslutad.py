1f42409695769467c821f33bd94482711ddecd46
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-21 16:49:18 +0100

    Update 06_Kommentarer oavslutad.py

--- pages/06_Kommentarer oavslutad.py
+++ pages/06_Kommentarer oavslutad.py
@@ -89,7 +89,8 @@ def remove_recommendations():
 
     for rubrik in unika_rubriker:
         st.subheader(rubrik)
-        filtered_items = list(filter(lambda person: person['Rubrik'] == rubrik, items))
+        filtered_items =\
+             list(filter(lambda item: item['Rubrik'] == rubrik, items))
         for item in filtered_items:
             st.write(item["Comment"])
             st.checkbox("Ta bort"
