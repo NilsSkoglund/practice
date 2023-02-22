5646da6867a2919a91e8b46b536e4d2325a141ea
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-22 15:21:40 +0100

    Update 04_Veckoschema.py

--- pages/04_Veckoschema.py
+++ pages/04_Veckoschema.py
@@ -13,12 +13,13 @@ st.session_state["lista_veckodagar"] = [
 ]
 if "deta" not in st.session_state:
     st.session_state["deta"] = Deta(st.secrets["deta_key"])
+db = st.session_state["deta"].Base("Veckoscheman")
 
 db_items = st.session_state["deta"].Base("Veckoscheman").fetch().items
 db_items = sorted(db_items, key=lambda x: int(x["key"]))
 
 def exercise_widgets_update_db(widget_str, week, day, workout):
-    db = st.session_state["deta"].Base("Veckoscheman")
+    
     db_item = db.get(week)
     if widget_str in ["Genomfört", "Kommentar"]:
         db_item[day][workout][widget_str] =\
@@ -27,6 +28,10 @@ def exercise_widgets_update_db(widget_str, week, day, workout):
         time_object = st.session_state[f"{widget_str}{week}{day}{workout}"]
         db_item[day][workout][widget_str]["timme"] = time_object.hour
         db_item[day][workout][widget_str]["minut"] = time_object.minute
+
+def update_db_weekly_note(key):
+    dct = {"note":st.session_state[key+"note"]}
+    db.update(dct, key)
 
 weeks = [i["key"] for i in db_items]
 
@@ -45,6 +50,11 @@ for week in weeks:
 for item in db_items:
     if item["key"] in display_weeks:
         st.header(f"Vecka {item['key']}")
+        st.text_area("Allmänna noteringar om veckan"
+                    , value = "..."
+                    , key = item["key"]+"note"
+                    , on_change=update_db_weekly_note
+                    , args=(item["key"]))
         for day in st.session_state["lista_veckodagar"]:
             if len(item[day]) > 0:
                 with st.expander(day):
