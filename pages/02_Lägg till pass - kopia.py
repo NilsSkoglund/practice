ba557c5466f76f03a945bb82cde77e4ce6b97b6d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-25 11:47:22 +0100

    Update 02_Lägg till pass - kopia.py

--- pages/02_Lägg till pass - kopia.py
+++ pages/02_Lägg till pass - kopia.py
@@ -3,9 +3,6 @@ from datetime import datetime
 from deta import Deta
 from funcs import helper_funcs
 
-choice = helper_funcs.options_menu()
-
-st.write(choice)
 
 ############################### Session state #################################
 if "deta" not in st.session_state:
@@ -16,3 +13,37 @@ table = "workouts_new"
 db = st.session_state["deta"].Base(table)
 
 ################################# Functions ###################################
+def workout_name():
+    name = st.text_input("Ange namn på pass")
+    workout_added = False
+    try:
+        db.insert(name)
+        workout_added = True
+    except:
+        st.info("Namn finns redan. Välj ett annat namn.")
+    return workout_added, name
+
+def update_db(name, exercises):
+    dct = {"Övningar": exercises}
+    db.update(dct, name)
+
+def workout_form(name):
+    with st.form(name):
+        exercises = st.text_area("Lägg in övningar")
+        submitted = st.form_submit_button("Skapa pass")
+        if submitted:
+            update_db(name, exercises)
+
+def add_workout():
+    workout_added, name = workout_name()
+    if workout_added:
+        workout_form(name)
+
+################################## Program ####################################
+
+choice = helper_funcs.options_menu()
+
+st.write(choice)
+
+if choice == "add":
+    add_workout()
