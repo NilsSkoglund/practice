2c0e48635c5945c18ac07bd68811e6766842c928
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-02-25 14:49:26 +0100

    Update 03_veckoschema ny.py

--- pages/03_veckoschema ny.py
+++ pages/03_veckoschema ny.py
@@ -22,6 +22,8 @@ lista_veckodagar = [
     , "lördag"
     , "söndag"
 ]
+db_workouts = st.session_state["deta"].Base("workouts_new")
+workouts = [i["Namn"] for i in db_workouts.fetch().items]
 
 ################################# Functions ###################################
 def options_menu():
@@ -98,10 +100,59 @@ def list_days():
                 , key=day
             )
 
+def add_workout_to_weekly_schedule(day, week):
+    
+    db_res = db.get(f"{week}")    
+
+    pass_namn =  st.session_state[f"selectbox_{day}"]
+
+    temp_dct = {"Genomfört": False
+            , "Kommentar": ""}
+
+    db_res[day].update({pass_namn: temp_dct})
+
+    db.put(db_res)
+
+def remove_workout_from_schedule(week, day, workout):
+    item = db.get(week)
+    del item[day][workout]
+    db.put(item)
+
+def loop_days(chosen_week):
+    for day in lista_veckodagar:
+
+        if st.session_state[day]:
+            with st.expander(day):
+                st.selectbox(""
+                    , options = workouts
+                    , key = f"selectbox_{day}"
+                    , on_change = add_workout_to_weekly_schedule
+                    , args = (day, chosen_week, )
+                    , label_visibility = "collapsed"
+                )
+
+                week_dct = db.get(chosen_week)
+                #st.markdown(f"**{day}**")
+                if len(week_dct[day].keys()) == 0:
+                    st.markdown("- Inget pass inlagt")
+                else:
+                    st.markdown("**Nedan visas inlagda pass.\
+                                 Klicka i boxen för att ta bort ett pass**")
+                    for workout in week_dct[day].keys():
+                        week = st.session_state["week_from_input"]
+                        key = f"Remove {week} {day} {workout}"
+                        st.checkbox(workout
+                                    , key=key
+                                    , on_change=remove_workout_from_schedule
+                                    , args=(week, day, workout))
+
+
 def add_weekly_schedule():
     chosen_week = choose_week()
     insert_weekly_schedule_db(chosen_week)
+    st.subheader(f"Vecka: {chosen_week}")
     list_days()
+    loop_days(chosen_week)
 
 ################################## Program ####################################
 st.subheader(f"Dagens datum: {datetime.now().date()}")
